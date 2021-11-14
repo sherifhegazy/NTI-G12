bb718a09ba083b556cede8d775dce204672fd1af
marwaMRadwan <marwaradwan872@hotmail.com>
2021-11-14 10:57:32 +0200

    s6

--- session6/controller/user.controller.js
+++ session6/controller/user.controller.js
@@ -1,4 +1,9 @@
-const test = (req, res)=> {
-    res.render("add")
+const add = (req,res) => {
+    res.render('add', {
+        pageTitle:"add new user"
+    })
 }
-module.exports= {test}
+
+module.exports = {
+    add
+}
